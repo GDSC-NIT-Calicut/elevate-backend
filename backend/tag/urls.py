bc229ab7174f9fc4219a4342f0340001b46fdc31
thomasjames433 <thomaspjames433@gmail.com>
2025-06-20 00:05:52 +0530

    [refactor]: modified urls

--- backend/tag/urls.py
+++ backend/tag/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns=[
     path('',ListCreateTag.as_view(),name='tag-list-create'),
-    path('<int:pk>/',TagDetail.as_view(),name='tag-detail')
+    path('/<int:pk>',TagDetail.as_view(),name='tag-detail')
 ]
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('google-oauth/',Signin.as_view(),name='Signin'),
-    path('backup-email/',Backup_Email.as_view(),name='Backup-Email'),
+    path('/google-oauth',Signin.as_view(),name='Signin'),
+    path('/backup-email',Backup_Email.as_view(),name='Backup-Email'),
     
 ]
