8869b0f80455d5dd7ed43b146b001d5f259ad08b
saninkaz <guardbro85@gmail.com>
2025-07-04 16:20:14 +0530

    [feat]: Add setting the student as SPOC OR PR functions

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns=[
     path('/google-oauth',Signin.as_view(),name='Signin'),
     path('/backup-email',Backup_Email.as_view(),name='Backup-Email'),
-    
+    path('/set-pr', SetPRRole.as_view(), name='set-pr'),
+    path('/set-spoc', SetSPOCRole.as_view(), name='set-spoc')
 ]
