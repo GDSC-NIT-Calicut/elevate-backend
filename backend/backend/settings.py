80578cc3457abec5dcae9a62715f3b2b19b37530
thomasjames433 <thomaspjames433@gmail.com>
2025-08-24 04:32:24 +0530

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -154,12 +154,16 @@ AUTH_USER_MODEL = 'user.User'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL"),  # Render gives you this environment variable
+        conn_max_age=600,                # keeps connections alive for performance
+        ssl_require=True                 # important: Render Postgres requires SSL
+    )
 }
+
 
 
 # Password validation
